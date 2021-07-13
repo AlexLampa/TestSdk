4bad3059471b6a1ce2ec1acf3474d6ceb20ae428
Oleksii Dundar <Oleksii.Dundar@gmail.com>
2021-07-13 23:54:07 +0300

    sadsasa

--- linb/src/main/java/com/test/linb/Hello.java
+++ linb/src/main/java/com/test/linb/Hello.java
@@ -1,4 +1,11 @@
 package com.test.linb;
 
+import com.test1307.alexlibrary.TestClass1;
+
 public class Hello {
+
+    private void metohd() {
+        TestClass1.Companion.showLogs("ewreweeeeeeeeeeeeeeeeeeeeeeeee");
+    }
+
 }
