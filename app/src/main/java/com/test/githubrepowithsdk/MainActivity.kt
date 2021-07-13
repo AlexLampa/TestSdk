05525dfba90a009db354ba3833b0a5cda3793ca4
Oleksii Dundar <Oleksii.Dundar@gmail.com>
2021-07-13 23:46:44 +0300

    sadsasa

--- app/src/main/java/com/test/githubrepowithsdk/MainActivity.kt
+++ app/src/main/java/com/test/githubrepowithsdk/MainActivity.kt
@@ -2,14 +2,12 @@ package com.test.githubrepowithsdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.test1307.alexlibrary.TestClass1
+import com.test.linb.Hello
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        TestClass1.showLogs("qwert")
 
     }
 }
---
+++ linb/src/main/java/com/test/linb/Hello.java
@@ -0,0 +1,4 @@
+package com.test.linb;
+
+public class Hello {
+}
