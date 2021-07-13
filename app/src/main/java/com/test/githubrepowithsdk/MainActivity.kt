7d728b8b665bab630b65de7fa3ec8350c3e71e8f
Oleksii Dundar <Oleksii.Dundar@gmail.com>
2021-07-13 23:09:37 +0300

    minor fix

--- app/src/main/java/com/test/githubrepowithsdk/MainActivity.kt
+++ app/src/main/java/com/test/githubrepowithsdk/MainActivity.kt
@@ -2,12 +2,14 @@ package com.test.githubrepowithsdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.myex2.TestClass2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        TestClass2().showToastt(this, "adas")
 
     }
 }
